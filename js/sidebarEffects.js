5bdfdb4509eae229279f98e3f9f54c9975988223
Matt Grimes <matt@rosemontmedia.com>
2014-08-29 16:12:46 -0700

    new slide

--- js/sidebarEffects.js
+++ js/sidebarEffects.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-
+	$('#menu').hide();
 $(".menulink, .closemenu").click(function(){
+	if (true) {
+		$("#menu").hasClass("active");
+			$('#menu').show(100);
+	}
+	else {
+		$('#menu').hide();
+	}
+
       $("#menu").toggleClass("active");
       $(".pushme").toggleClass("active");
 });
